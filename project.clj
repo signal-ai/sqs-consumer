e6cc9ffd73a391d186dc129e9d7908127e00886b
Hugo Firth <hugo.firth@gmail.com>
2019-07-09 17:27:43 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqs-consumer "0.1.0"
+(defproject sqs-consumer "0.1.1"
   :description "Another SQS Library"
   :url "https://github.com/signal-ai/sqs-consumer"
   :license {:name "Eclipse Public License"
