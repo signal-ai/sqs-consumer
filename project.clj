02cb875d69a9c1d3d5f4d5f024a6a2a8654032e7
Hugo Firth <hugo.firth@gmail.com>
2020-09-09 14:08:34 +0100

    Adding config to deploy to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqs-consumer "0.1.4"
+(defproject sqs-consumer "0.1.6-SNAPSHOT"
   :description "Another SQS Library"
   :url "https://github.com/signal-ai/sqs-consumer"
   :license {:name "Eclipse Public License"
@@ -7,4 +7,10 @@
                  [com.amazonaws/aws-java-sdk-sqs "1.11.475"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.clojure/data.json "0.2.6"]
-                                  [greenpowermonitor/test-doubles "0.1.2"]]}})
+                                  [greenpowermonitor/test-doubles "0.1.2"]]}}
+   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password}]
+                         ["snapshots" {:url "https://clojars.org/repo"
+                                       :username :env/clojars_username
+                                       :password :env/clojars_password}]])
